07c0d955e4d8386f83a0f77de8a9f255afd6fac3
C4 Patino <c4patino@gmail.com>
2022-03-04 17:27:53 -0600

    Outlined basic route checklist

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,25 @@ router.get('/', (req, res, next) => {
     res.render('index', { title: 'Express' });
 });
 
+/* 
+User
+- Create user
+- Change password
+- User Login
+- User Profile
+- Modify user profile
+
+Organization
+- Create organization
+- Create organization account
+- Add people to organization
+- Remove people from organization
+
+Groups
+- Create group
+- Add people to group
+- Remove people from group
+- Delete group
+*/
+
 module.exports = router;
