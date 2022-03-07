e741cf400797075dc57f0a1405df7b7583fcdb6b
C4 Patino <c4patino@gmail.com>
2022-03-06 20:40:55 -0600

    Removed TODO list from GitHub

--- server/routes/auth/Logout.js
+++
@@ -1,9 +0,0 @@
-async function Logout(req, res, next) {
-    const user = req.user;
-    const token = await user.getToken();
-    await token.destroy();
-
-    return res.send('User logged out');
-}
-
-module.exports = Logout;
