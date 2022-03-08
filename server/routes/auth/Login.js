e26fd3e974920d8382572823db6e2b624e549bfe
C4 Patino <c4patino@gmail.com>
2022-03-08 16:09:42 -0600

    Reconfigured remember parameter

--- server/routes/auth/Login.js
+++ server/routes/auth/Login.js
@@ -2,7 +2,7 @@ const forge = require('node-forge');
 const { Token } = require('../../models/index');
 
 async function Login(req, res, next) {
-    const remember = req.remember;
+    const remember = req.body.remember;
 
     await Token.destroy({ where: { userID: req.user.userID } });
     const newToken = await Token.create({
