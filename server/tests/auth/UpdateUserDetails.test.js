633cef9b1098e264f73ec8753ca1e93f788dbce9
C4 Patino <c4patino@gmail.com>
2022-03-27 18:25:13 -0500

    Completed UpdateUserDetails test suite

---
+++ server/tests/auth/UpdateUserDetails.test.js
@@ -0,0 +1,66 @@
+require('dotenv').config();
+const supertest = require('supertest');
+
+var { sequelize } = require('../../models/index');
+const app = require('../../app');
+const { createTestToken } = require('../utils');
+
+describe('UpdateUserDetails', function () {
+    beforeEach(async () => {
+        try {
+            await sequelize.authenticate();
+            await sequelize.sync({ force: 'true' });
+        } catch (error) {
+            console.log('[ERROR]: Database connection failed');
+        }
+    });
+
+    test('[200] Successfully changed user details', async () => {
+        const token = await createTestToken({
+            firstName: 'Test',
+            lastName: 'User',
+            password: 'password',
+        });
+
+        await supertest(app)
+            .patch('/api/auth/update')
+            .send({
+                firstName: 'New',
+                lastName: 'User',
+                email: 'new.user@test.com',
+                token: token.tokenID,
+            })
+            .expect(200)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .then(async (response) => {
+                expect(response.body).toEqual(
+                    expect.objectContaining({
+                        firstName: 'New',
+                        lastName: 'User',
+                        email: 'new.user@test.com',
+                    })
+                );
+            });
+    });
+
+    test('[500] Pre-existing user with email', async () => {});
+
+    test('[403] Missing token', async () => {
+        await supertest(app)
+            .patch('/api/auth/update')
+            .send()
+            .expect(403, 'Unauthorized user')
+            .set('Accept', 'text/html')
+            .expect('Content-Type', /text/);
+    });
+
+    test('[511] Token was not found', async () => {
+        await supertest(app)
+            .patch('/api/auth/update')
+            .send({ token: 'randomString' })
+            .expect(511, 'Session expired')
+            .set('Accept', 'text/html')
+            .expect('Content-Type', /text/);
+    });
+});
