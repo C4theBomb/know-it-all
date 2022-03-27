770074ea023cfed9feb2ceadd933f335cd3f9080
C4 Patino <c4patino@gmail.com>
2022-03-27 13:21:23 -0500

    Added direct join functionality

--- server/routes/auth/CreateUser.js
+++ server/routes/auth/CreateUser.js
@@ -1,5 +1,5 @@
 const forge = require('node-forge');
-const { User } = require('../../models/index');
+const { User, Organization } = require('../../models/index');
 
 async function CreateUser(req, res, next) {
     const body = req.body;
@@ -18,19 +18,41 @@ async function CreateUser(req, res, next) {
         return res.status(500).send('A user with that email already exists.');
     }
 
-    try {
-        const passwordHash = forge.md.sha512
-            .create()
-            .update(body.password)
-            .digest()
-            .toHex();
-        const result = await User.create({ ...body, password: passwordHash });
+    const passwordHash = forge.md.sha512
+        .create()
+        .update(body.password)
+        .digest()
+        .toHex();
 
-        return res.send(result);
-    } catch (e) {
-        console.log(e);
+    if (body.orgID) {
+        try {
+            const org = await Organization.findByPk(body.orgID);
+            const { orgID, ...filteredBody } = body;
 
-        return res.status(500).send('Whoops, something went wrong!');
+            const result = await org.createOrgMember({
+                ...filteredBody,
+                password: passwordHash,
+            });
+
+            return res.send(result);
+        } catch (e) {
+            console.log(e);
+
+            return res.status(500).send('Whoops, something went wrong!');
+        }
+    } else {
+        try {
+            const result = await User.create({
+                ...body,
+                password: passwordHash,
+            });
+
+            return res.send(result);
+        } catch (e) {
+            console.log(e);
+
+            return res.status(500).send('Whoops, something went wrong!');
+        }
     }
 }
 
