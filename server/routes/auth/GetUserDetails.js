586567690d848e6460f57a818f22c38e252a726f
C4 Patino <c4patino@gmail.com>
2022-12-15 13:42:30 -0600

    Updated server

--- server/routes/auth/GetUserDetails.js
+++ server/routes/auth/GetUserDetails.js
@@ -1,13 +1,7 @@
 const { User } = require('../../db/models/index');
 
 async function GetUserDetails(req, res, next) {
-    const user = req.user;
     const userID = req.params.userID;
-
-    // Immediately respond if user is querying themselves
-    if (user.id == userID) {
-        return res.send({ user });
-    }
 
     // Retrieve user while excluding sensitive information
     const result = await User.findOne({
---
+++ server/routes/auth/Remember.js
@@ -0,0 +1,9 @@
+async function Remember(req, res) {
+    const user = req.user;
+
+    return res.send({
+        user,
+    });
+}
+
+module.exports = Remember;
